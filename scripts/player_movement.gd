a53bf1941ed1bce92877000f9b28e08a5387514e
Mattias Selin <mattias.selin@gmail.com>
2025-07-12 19:25:24 +0200

    Change Police camera

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @onready var revealButton: Sprite2D = get_node("Camera2D/RevealButton")
-@onready var nitroBoostButton: Sprite2D = get_node("Camera2D/NitroBoost")
+@onready var nitroBoostButton: Sprite2D = get_node("PoliceCamera2D/NitroBoost")
+@onready var rebelCamera: Camera2D = get_node("Camera2D")
+@onready var policeCamera: Camera2D = get_node("PoliceCamera2D")
 @onready var player = get_node("CharacterBody2D")
 
 func _ready() -> void:
 	if ClientPlayer.role != Lobby.ROLE_REBEL:
 		revealButton.get_parent().remove_child(revealButton)
+		rebelCamera.enabled = false
+		policeCamera.enabled = true
 	if ClientPlayer.role != Lobby.ROLE_POLICE:
 		nitroBoostButton.get_parent().remove_child(nitroBoostButton)
 	
