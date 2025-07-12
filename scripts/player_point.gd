266ea6d5501d7d85045c7fadcc88344646edd36f
ulraf <nir.vaknin@mojang.com>
2025-07-12 14:51:51 +0200

    The camera's center doesn't leave the map

--- scripts/player_point.gd
+++ scripts/player_point.gd
@@ -43,10 +43,20 @@ func _physics_process(_delta: float) -> void:
 
 	var normalized_viewport := get_viewport().get_canvas_transform().affine_inverse()*get_viewport_rect()
 	normalized_viewport.position = target_camera_pos - normalized_viewport.size/2
-	var map_bounds := map.transform*map.get_rect();
-
-	if map_bounds.encloses(normalized_viewport):
-		camera.global_position = target_camera_pos
+	var map_half_width = map.get_rect().size.x / 2
+	var map_half_height = map.get_rect().size.y / 2
+	
+	target_camera_pos.x = clamp(
+		target_camera_pos.x,
+		map.position.x - map_half_width,
+		map.position.x + map_half_width
+	)
+	target_camera_pos.y = clamp(
+		target_camera_pos.y,
+		map.position.y - map_half_height,
+		map.position.y + map_half_height
+	)
+	camera.global_position = target_camera_pos
 	
 	Lobby.update_position.rpc_id(1, global_position)
 
