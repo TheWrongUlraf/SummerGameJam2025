2ad835cc6a1302cf00b223b0650c931e163fbfb8
Mattias Selin <mattias.selin@gmail.com>
2025-07-13 18:24:51 +0200

    'Music'

--- scripts/lobby_screen.gd
+++ scripts/lobby_screen.gd
@@ -4,6 +4,7 @@ func _ready():
 	if Lobby.is_server():
 		$VerticalBox/ClientControls.hide()
 		$VerticalBox/ServerControls.show()
+		$MainMusic.play()
 	else:
 		$VerticalBox/ClientControls.show()
 		$VerticalBox/ServerControls.hide()
