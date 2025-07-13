fa8801f55013a1e1bc8f1ae08faa0989007f9ffe
Pavel Grebnev <ruwhynot@gmail.com>
2025-07-13 08:46:51 +0200

    Rename hud elements in ingame hud

--- scripts/in_game_hud.gd
+++ scripts/in_game_hud.gd
@@ -1,16 +1,16 @@
 extends Control
 
 func _ready():
-	if Lobby.client_get_role() == Lobby.ROLE_POLICE:
-		$RebelHud/HBoxContainer/VBoxContainer.hide()
-		return
-
-	$RebelHud/HBoxContainer/VBoxContainer/Emoji1Button.on_pressed.connect(_on_emoji1_pressed)
-	$RebelHud/HBoxContainer/VBoxContainer/Emoji2Button.on_pressed.connect(_on_emoji2_pressed)
-	$RebelHud/HBoxContainer/VBoxContainer/Emoji3Button.on_pressed.connect(_on_emoji3_pressed)
-	
 	Lobby.client_on_stage_changed.connect(_on_stage_changed)
 	_on_stage_changed(Lobby.client_get_stage(), Lobby.client_get_stage_objective_icon())
+
+	if Lobby.client_get_role() == Lobby.ROLE_POLICE:
+		$RebelHud/Panels/RightControlsPanel.hide()
+		return
+
+	$RebelHud/Panels/RightControlsPanel/Emoji1Button.on_pressed.connect(_on_emoji1_pressed)
+	$RebelHud/Panels/RightControlsPanel/Emoji2Button.on_pressed.connect(_on_emoji2_pressed)
+	$RebelHud/Panels/RightControlsPanel/Emoji3Button.on_pressed.connect(_on_emoji3_pressed)
 
 
 func _on_emoji1_pressed():
@@ -27,14 +27,17 @@ func _on_emoji3_pressed():
 
 func _place_emoji_on_map(index):
 	Lobby.server_place_emoji_on_map.rpc_id(1, multiplayer.get_unique_id(), index)
-	$RebelHud/HBoxContainer/VBoxContainer.hide()
+	$RebelHud/Panels/RightControlsPanel.hide()
 	$EmojiCooldownTimer.start(Lobby.EMOJI_COOLDOWN_TIME_SEC)
 
 
 func _on_emoji_cooldown_timer_timeout() -> void:
-	$RebelHud/HBoxContainer/VBoxContainer.show()
+	$RebelHud/Panels/RightControlsPanel.show()
 
 
 func _on_stage_changed(stage, icon):
-	$RebelHud/HBoxContainer/VBoxContainer2/StageNumberText.text = "Stage " + str(stage + 1)
-	$RebelHud/HBoxContainer/VBoxContainer2/HBoxContainer/ObjectiveIcon.texture = Lobby.EMOJI_TEXTURES[icon]
+	$RebelHud/Panels/ObjectivePanel/StageNumberText.text = "Stage " + str(stage + 1)
+	if Lobby.client_get_role() == Lobby.ROLE_REBEL:
+		$RebelHud/Panels/ObjectivePanel/ObjectiveRow/ObjectiveIcon.texture = Lobby.EMOJI_TEXTURES[icon]
+	else:
+		$RebelHud/Panels/ObjectivePanel/ObjectiveRow.hide()
